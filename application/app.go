839a63242c9c76c35225f66a33c870cb58bc15d9
付云帆 <fuyunfan@ihawk.cn>
2024-01-10 17:10:50 +0800

    修改配置读取方式的bug

--- application/app.go
+++ application/app.go
@@ -244,12 +244,12 @@ func BindFlags(ctx context.Context, groupName string, set *pflag.FlagSet, v *vip
 			}
 		}
 		// use default
-		if !f.Changed {
-			err := set.Set(f.Name, f.DefValue)
-			if err != nil {
-				logger.Error(ctx, "bind flag error", err, logger.WithField("groupName", groupName), logger.WithField("flagName", f.Name), logger.WithField("setVal", f.DefValue))
-			}
-		}
+		//if !f.Changed {
+		//	err := set.Set(f.Name, f.DefValue)
+		//	if err != nil {
+		//		logger.Error(ctx, "bind flag error", err, logger.WithField("groupName", groupName), logger.WithField("flagName", f.Name), logger.WithField("setVal", f.DefValue))
+		//	}
+		//}
 	}
 	set.VisitAll(call)
 }
